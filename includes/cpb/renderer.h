09a9549f4b658383a2aa052a8267d6c0901b01b9
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:43:53 +0330

    Add function declarations for renderer operations

--- includes/cpb/renderer.h
+++ includes/cpb/renderer.h
@@ -1,1 +1,14 @@
+#ifndef CPB_RENDERER_H
+#define CPB_RENDERER_H
+
+/* Register a bar for rendering */
+void cpb_renderer_register_bar(void* bar);
+
+/* Render all components */
+void cpb_renderer_render_all(void);
+
+/* Clear renderer state */
+void cpb_renderer_clear(void);
+
+#endif /* CPB_RENDERER_H */
 
