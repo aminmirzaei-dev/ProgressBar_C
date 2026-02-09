9565f6a346f73eda57f0d1c9a9cc07e259c656ca
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:45:03 +0330

    Implement terminal control functions

--- sources/terminal/terminal_ansi.c
+++ sources/terminal/terminal_ansi.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "../../include/cpb/terminal.h"
+
+void cpb_terminal_clear(void) {
+    printf("\033[2J");
+}
+
+void cpb_terminal_move_cursor_home(void) {
+    printf("\033[H");
+}
+
+void cpb_terminal_hide_cursor(void) {
+    printf("\033[?25l");
+}
+
+void cpb_terminal_show_cursor(void) {
+    printf("\033[?25h");
+}
 
