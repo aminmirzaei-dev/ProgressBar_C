69b9956ec727c641f68eacd2bbf8294260e2ab30
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:47:16 +0330

    Implement CPB library initialization and rendering
    Added functions to initialize, render, and shutdown the CPB library.

--- sources/core/cpb.c
+++ sources/core/cpb.c
@@ -1,1 +1,29 @@
+#include "../../include/cpb/cpb.h"
+#include "../../include/cpb/renderer.h"
+#include "../../include/cpb/terminal.h"
+
+/*
+ * Initialize CPB library
+ * Clears the terminal and prepares rendering environment
+ */
+void cpb_init(void) {
+    cpb_terminal_clear();
+    cpb_terminal_move_cursor_home();
+    cpb_terminal_hide_cursor();
+}
+
+/*
+ * Render all registered UI components
+ */
+void cpb_render(void) {
+    cpb_renderer_render_all();
+}
+
+/*
+ * Restore terminal state and cleanup resources
+ */
+void cpb_shutdown(void) {
+    cpb_terminal_show_cursor();
+    cpb_renderer_clear();
+}
 
