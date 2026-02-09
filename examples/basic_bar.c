f875fc3338afe9b0f3199e4fc0f3591b793df389
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:45:41 +0330

    Add basic progress bar example in basic_bar.c

--- examples/basic_bar.c
+++ examples/basic_bar.c
@@ -1,1 +1,20 @@
+#include <unistd.h>
+#include "../include/cpb/cpb.h"
+
+int main() {
+    cpb_init();
+
+    cpb_bar_t* bar = cpb_bar_create(100);
+    cpb_bar_set_label(bar, "Downloading");
+    cpb_bar_set_width(bar, 40);
+
+    for (int i = 0; i <= 100; i++) {
+        cpb_bar_set(bar, i);
+        cpb_render();
+        usleep(50000);
+    }
+
+    cpb_shutdown();
+    return 0;
+}
 
