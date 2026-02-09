18181f4aa916ca6c2bcdb1f0845e85522b3cc576
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:41:30 +0330

    Add header file for CPB library functions

--- includes/cpb/cpb.h
+++ includes/cpb/cpb.h
@@ -1,1 +1,26 @@
+#ifndef CPB_H
+#define CPB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "bar.h"
+#include "renderer.h"
+#include "terminal.h"
+
+/* Initialize the library (must be called once) */
+void cpb_init(void);
+
+/* Render all registered components */
+void cpb_render(void);
+
+/* Cleanup terminal state */
+void cpb_shutdown(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CPB_H */
 
