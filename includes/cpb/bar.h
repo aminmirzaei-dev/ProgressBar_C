287aa97b2e980a816cd2a66c2b193a30ef102684
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:43:33 +0330

    Create bar.h for progress bar interface
    Added header file for progress bar functionality.

--- includes/cpb/bar.h
+++ includes/cpb/bar.h
@@ -1,1 +1,33 @@
+#ifndef CPB_BAR_H
+#define CPB_BAR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct cpb_bar cpb_bar_t;
+
+/* Create a progress bar with a maximum value */
+cpb_bar_t* cpb_bar_create(int max);
+
+/* Set bar properties */
+void cpb_bar_set_label(cpb_bar_t* bar, const char* label);
+void cpb_bar_set_width(cpb_bar_t* bar, int width);
+
+/* Progress control */
+void cpb_bar_set(cpb_bar_t* bar, int value);
+void cpb_bar_increment(cpb_bar_t* bar, int delta);
+int  cpb_bar_is_finished(cpb_bar_t* bar);
+
+/* Destroy bar */
+void cpb_bar_destroy(cpb_bar_t* bar);
+
+/* Internal: render bar */
+void cpb_bar_render(const cpb_bar_t* bar);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CPB_BAR_H */
 
