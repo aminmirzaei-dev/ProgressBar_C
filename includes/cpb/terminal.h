bc0d60925f23cf07db86ac685b7c2830d2e703ba
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-09 03:44:06 +0330

    Add terminal control functions to terminal.h

--- includes/cpb/terminal.h
+++ includes/cpb/terminal.h
@@ -1,1 +1,11 @@
+#ifndef CPB_TERMINAL_H
+#define CPB_TERMINAL_H
+
+/* Terminal control (ANSI based) */
+void cpb_terminal_clear(void);
+void cpb_terminal_move_cursor_home(void);
+void cpb_terminal_hide_cursor(void);
+void cpb_terminal_show_cursor(void);
+
+#endif /* CPB_TERMINAL_H */
 
